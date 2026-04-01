1aaea5c1fa50ba5ad0f742d3fb741aa2ea587936
Gabriel Gomes <gggabriel368@gmail.com>
2026-04-01 17:07:05 -0300

    style created

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'learning_logs',
     'users',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 LOGIN_URL = '/users/login/'
+
+BOOTSTRAP3 = {
+    'include_jquery': True,
+}
